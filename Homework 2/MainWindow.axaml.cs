088cf755119e36193982fddf647b3d779150e323
harakki <124341753+harakki@users.noreply.github.com>
2024-02-10 16:22:06 +0700

    Finish homework 2 by creating an application with a graphical interface

--- Homework 2/MainWindow.axaml.cs
+++ Homework 2/MainWindow.axaml.cs
@@ -1,11 +1,30 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Interactivity;
 
 namespace Homework_2;
 
 public partial class MainWindow : Window
 {
+    private readonly Rectangle? _rectangle;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        Width = 600;
+        Height = 400;
+
+        MinWidth = 300;
+        MinHeight = 300;
+
+        _rectangle = this.FindControl<Rectangle>("Rectangle");
+    }
+
+    private void ClickHandler(object sender, RoutedEventArgs args)
+    {
+        var clickedButton = (Button)sender;
+        var buttonColor = clickedButton.Background;
+        if (_rectangle != null) _rectangle.Fill = buttonColor;
     }
 }
